877e28e2f2bd4c76367acdfb7c938cf9705dbfcd
Qendresa Hoti <qendresahhoti@gmail.com>
2025-09-05 16:34:15 +0100

    comment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,14 +43,14 @@ export default function Header(): ReactElement {
                   /about
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   href="/blog"
                   className="hover:text-[#ff79c6] focus:outline-none focus:text-[#ff79c6]"
                 >
                   /blog
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <Link
                   href="/projects"
